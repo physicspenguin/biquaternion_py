bdc69f32ab592ca847c8cceea0f8aee60603c7db
Daren Thimm <daren.thimm@uibk.ac.at>
2023-07-03 12:11:10 +0200

    Cleanup, adding of conjugations and quadrance

--- biquat_py/biquaternion.py
+++ biquat_py/biquaternion.py
@@ -1,29 +1,58 @@
-global _bq_i, _bq_j, _bq_e
+"""
+This module implements the biquaternion class, which takes care of
+ the basic biquaternion arithmetic.
+"""
+
 import numpy as np
 
-_bq_i = -1
-_bq_j = -1
-_bq_e = 0
+_BQ_I = -1
+_BQ_J = -1
+_BQ_E = 0
 
 
-def define_algebra(a=-1, b=-1, c=0):
-    _bq_i = a
-    _bq_j = b
-    _bq_e = c
-    II = biquaternion([0, 1, 0, 0, 0, 0, 0, 0])
-    JJ = biquaternion([0, 0, 1, 0, 0, 0, 0, 0])
-    KK = biquaternion([0, 0, 0, 1, 0, 0, 0, 0])
-    EE = biquaternion([0, 0, 0, 0, 1, 0, 0, 0])
+def define_algebra(i_square=-1, j_square=-1, e_square=0):
+    """ Defines the algebra
+
+    Parameters
+    ----------
+    i_square : float
+        value of II**2
+    j_square : float
+        value of JJ**2
+    e_square : float
+        value of EE**2
+
+    Returns
+    -------
+    None
+
+    Notes
+    -----
+    The algebra of bi-quaternions is fully defined by the following conditions:
+    .. math:: i^2 = a, j^2 = b, e^2 = c,\\
+    ij = k, ji = -k,\\
+    ei = ie, ej = je, ek = ke
+    All objects commute with elements of the chosen base field.
+    Mostly this is :math:`\mathbb{R}` or :math:`\mathbb{C}`.
+    """
+    global _BQ_I, _BQ_J, _BQ_E, II, JJ, KK, EE
+    _BQ_I = i_square
+    _BQ_J = j_square
+    _BQ_E = e_square
+    II = BiQuaternion([0, 1, 0, 0, 0, 0, 0, 0])
+    JJ = BiQuaternion([0, 0, 1, 0, 0, 0, 0, 0])
+    KK = BiQuaternion([0, 0, 0, 1, 0, 0, 0, 0])
+    EE = BiQuaternion([0, 0, 0, 0, 1, 0, 0, 0])
 
 
-class biquaternion:
+class BiQuaternion:
 
     """Biquaternions as $a + II b + JJ c + KK d + EE (w + II x + JJ y + KK z)$"""
 
     coeff = [0, 0, 0, 0, 0, 0, 0, 0]
 
     def __init__(self, gen):
-        if isinstance(gen, biquaternion):
+        if isinstance(gen, BiQuaternion):
             cof = gen.coeff
         elif isinstance(gen, (list, tuple, np.ndarray)):
             if len(gen) >= 9:
@@ -34,64 +63,64 @@ class biquaternion:
 
         self.coeff = [0, 0, 0, 0, 0, 0, 0, 0]
 
-        for i in range(len(cof)):
-            self.coeff[i] = cof[i]
+        for i, val in enumerate(cof):
+            self.coeff[i] = val
 
     def __mul__(self, other):
-        if isinstance(other, biquaternion):
+        if isinstance(other, BiQuaternion):
             out = [
-                -_bq_e * _bq_i * _bq_j * other.coeff[7] * self.coeff[7]
-                + _bq_e * _bq_i * other.coeff[5] * self.coeff[5]
-                + _bq_e * _bq_j * other.coeff[6] * self.coeff[6]
-                - _bq_i * _bq_j * other.coeff[3] * self.coeff[3]
-                + _bq_e * other.coeff[4] * self.coeff[4]
-                + _bq_i * other.coeff[1] * self.coeff[1]
-                + _bq_j * other.coeff[2] * self.coeff[2]
+                -_BQ_E * _BQ_I * _BQ_J * other.coeff[7] * self.coeff[7]
+                + _BQ_E * _BQ_I * other.coeff[5] * self.coeff[5]
+                + _BQ_E * _BQ_J * other.coeff[6] * self.coeff[6]
+                - _BQ_I * _BQ_J * other.coeff[3] * self.coeff[3]
+                + _BQ_E * other.coeff[4] * self.coeff[4]
+                + _BQ_I * other.coeff[1] * self.coeff[1]
+                + _BQ_J * other.coeff[2] * self.coeff[2]
                 + other.coeff[0] * self.coeff[0],
-                _bq_e * _bq_j * other.coeff[6] * self.coeff[7]
-                - _bq_e * _bq_j * other.coeff[7] * self.coeff[6]
-                + _bq_e * other.coeff[4] * self.coeff[5]
-                + _bq_e * other.coeff[5] * self.coeff[4]
-                + _bq_j * other.coeff[2] * self.coeff[3]
-                - _bq_j * other.coeff[3] * self.coeff[2]
+                _BQ_E * _BQ_J * other.coeff[6] * self.coeff[7]
+                - _BQ_E * _BQ_J * other.coeff[7] * self.coeff[6]
+                + _BQ_E * other.coeff[4] * self.coeff[5]
+                + _BQ_E * other.coeff[5] * self.coeff[4]
+                + _BQ_J * other.coeff[2] * self.coeff[3]
+                - _BQ_J * other.coeff[3] * self.coeff[2]
                 + other.coeff[0] * self.coeff[1]
                 + other.coeff[1] * self.coeff[0],
-                -_bq_e * _bq_i * other.coeff[5] * self.coeff[7]
-                + _bq_e * _bq_i * other.coeff[7] * self.coeff[5]
-                + _bq_e * other.coeff[4] * self.coeff[6]
-                + _bq_e * other.coeff[6] * self.coeff[4]
-                - _bq_i * other.coeff[1] * self.coeff[3]
-                + _bq_i * other.coeff[3] * self.coeff[1]
+                -_BQ_E * _BQ_I * other.coeff[5] * self.coeff[7]
+                + _BQ_E * _BQ_I * other.coeff[7] * self.coeff[5]
+                + _BQ_E * other.coeff[4] * self.coeff[6]
+                + _BQ_E * other.coeff[6] * self.coeff[4]
+                - _BQ_I * other.coeff[1] * self.coeff[3]
+                + _BQ_I * other.coeff[3] * self.coeff[1]
                 + other.coeff[0] * self.coeff[2]
                 + other.coeff[2] * self.coeff[0],
-                _bq_e * other.coeff[4] * self.coeff[7]
-                - _bq_e * other.coeff[5] * self.coeff[6]
-                + _bq_e * other.coeff[6] * self.coeff[5]
-                + _bq_e * other.coeff[7] * self.coeff[4]
+                _BQ_E * other.coeff[4] * self.coeff[7]
+                - _BQ_E * other.coeff[5] * self.coeff[6]
+                + _BQ_E * other.coeff[6] * self.coeff[5]
+                + _BQ_E * other.coeff[7] * self.coeff[4]
                 + other.coeff[0] * self.coeff[3]
                 - other.coeff[1] * self.coeff[2]
                 + other.coeff[2] * self.coeff[1]
                 + other.coeff[3] * self.coeff[0],
-                -_bq_i * _bq_j * other.coeff[3] * self.coeff[7]
-                - _bq_i * _bq_j * other.coeff[7] * self.coeff[3]
-                + _bq_i * other.coeff[1] * self.coeff[5]
-                + _bq_i * other.coeff[5] * self.coeff[1]
-                + _bq_j * other.coeff[2] * self.coeff[6]
-                + _bq_j * other.coeff[6] * self.coeff[2]
+                -_BQ_I * _BQ_J * other.coeff[3] * self.coeff[7]
+                - _BQ_I * _BQ_J * other.coeff[7] * self.coeff[3]
+                + _BQ_I * other.coeff[1] * self.coeff[5]
+                + _BQ_I * other.coeff[5] * self.coeff[1]
+                + _BQ_J * other.coeff[2] * self.coeff[6]
+                + _BQ_J * other.coeff[6] * self.coeff[2]
                 + other.coeff[0] * self.coeff[4]
                 + other.coeff[4] * self.coeff[0],
-                _bq_j * other.coeff[2] * self.coeff[7]
-                - _bq_j * other.coeff[3] * self.coeff[6]
-                + _bq_j * other.coeff[6] * self.coeff[3]
-                - _bq_j * other.coeff[7] * self.coeff[2]
+                _BQ_J * other.coeff[2] * self.coeff[7]
+                - _BQ_J * other.coeff[3] * self.coeff[6]
+                + _BQ_J * other.coeff[6] * self.coeff[3]
+                - _BQ_J * other.coeff[7] * self.coeff[2]
                 + other.coeff[0] * self.coeff[5]
                 + other.coeff[1] * self.coeff[4]
                 + other.coeff[4] * self.coeff[1]
                 + other.coeff[5] * self.coeff[0],
-                -_bq_i * other.coeff[1] * self.coeff[7]
-                + _bq_i * other.coeff[3] * self.coeff[5]
-                - _bq_i * other.coeff[5] * self.coeff[3]
-                + _bq_i * other.coeff[7] * self.coeff[1]
+                -_BQ_I * other.coeff[1] * self.coeff[7]
+                + _BQ_I * other.coeff[3] * self.coeff[5]
+                - _BQ_I * other.coeff[5] * self.coeff[3]
+                + _BQ_I * other.coeff[7] * self.coeff[1]
                 + other.coeff[0] * self.coeff[6]
                 + other.coeff[2] * self.coeff[4]
                 + other.coeff[4] * self.coeff[2]
@@ -105,22 +134,22 @@ class biquaternion:
                 + other.coeff[6] * self.coeff[1]
                 + other.coeff[7] * self.coeff[0],
             ]
-            return biquaternion(out)
-        else:
-            return self * biquaternion(other)
+            return BiQuaternion(out)
+
+        return self * BiQuaternion(other)
 
     def __pos__(self):
-        return biquaternion(self)
+        return BiQuaternion(self)
 
     def __neg__(self):
-        return biquaternion([-self.coeff[i] for i in range(8)])
+        return BiQuaternion([-self.coeff[i] for i in range(8)])
 
     def __add__(self, other):
-        if isinstance(other, biquaternion):
+        if isinstance(other, BiQuaternion):
             out = [self.coeff[i] + other.coeff[i] for i in range(8)]
-            return biquaternion(out)
-        else:
-            return self + biquaternion(other)
+            return BiQuaternion(out)
+
+        return self + BiQuaternion(other)
 
     def __sub__(self, other):
         return self + (-other)
@@ -129,7 +158,6 @@ class biquaternion:
         return other + (-self)
 
     __radd__ = __add__
-    __rsub__ = __sub__
     __rmul__ = __mul__
 
     def __repr__(self):
@@ -160,8 +188,47 @@ class biquaternion:
         )
         return result
 
+    def conjugate(self):
+        """(Bi)-Quaternion conjugate of the quaternion.
 
-II = biquaternion([0, 1, 0, 0, 0, 0, 0, 0])
-JJ = biquaternion([0, 0, 1, 0, 0, 0, 0, 0])
-KK = biquaternion([0, 0, 0, 1, 0, 0, 0, 0])
-EE = biquaternion([0, 0, 0, 0, 1, 0, 0, 0])
+        Conjugation of a quaternion inverts the sign of the non-scalar part of
+        a (bi-)quaternion.
+        This happens in the same fashion as for complex numbers.
+        """
+        return BiQuaternion([self.coeff[0], -self.coeff[1],
+                             -self.coeff[2], -self.coeff[3],
+                             self.coeff[4], -self.coeff[5],
+                             -self.coeff[6], -self.coeff[7]])
+
+    def eps_conjugate(self):
+        """Epsilon conjugation of the biquaternion
+
+        Epsilon conjugation inverts the sign of the dual part of a quaternion
+        """
+        return BiQuaternion([self.coeff[0], self.coeff[1],
+                             self.coeff[2], self.coeff[3],
+                             -self.coeff[4], -self.coeff[5],
+                             -self.coeff[6], -self.coeff[7]])
+
+    def quadrance(self):
+        """Quadrance of a quaternion
+
+        The quadrance of a biquaternion is its norm. It is defined as
+        the product of a biquaternion and its conjugate.
+        """
+        return self*(self.conjugate())
+
+    def __invert__(self):
+        """(Bi)-Quaternion conjugate of the quaternion.
+
+        Conjugation of a quaternion changes the sign of the non-scalar part of
+        a (bi-)quaternion.
+        This happens in the same fashion as for complex numbers.
+        """
+        return self.conjugate()
+
+
+II = BiQuaternion([0, 1, 0, 0, 0, 0, 0, 0])
+JJ = BiQuaternion([0, 0, 1, 0, 0, 0, 0, 0])
+KK = BiQuaternion([0, 0, 0, 1, 0, 0, 0, 0])
+EE = BiQuaternion([0, 0, 0, 0, 1, 0, 0, 0])
