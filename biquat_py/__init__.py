2a279098646c672af2a8f4fc3228ebb243b6edbf
Daren Thimm <daren.thimm@uibk.ac.at>
2023-07-27 12:28:16 +0200

    added new functions to init.

--- biquat_py/__init__.py
+++ biquat_py/__init__.py
@@ -1,2 +1,3 @@
 from .biquaternion import define_algebra, BiQuaternion, II, JJ, KK, EE
-from .polynomials import Poly
+from .polynomials import Poly, poly_div
+from .lines import quaternion_to_pluecker, pluecker_to_quaternion
