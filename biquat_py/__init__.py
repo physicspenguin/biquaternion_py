47d546453120ef6f425bb83574f0bcb3b5aee571
Daren Thimm <daren.thimm@uibk.ac.at>
2023-07-28 12:00:19 +0200

    Added random generation to init

--- biquat_py/__init__.py
+++ biquat_py/__init__.py
@@ -2,3 +2,4 @@ from .biquaternion import define_algebra, BiQuaternion, II, JJ, KK, EE
 from .polynomials import Poly, poly_div
 from .lines import quaternion_to_pluecker, pluecker_to_quaternion
 from .poly_tools import max_real_poly_fact, gcd_conj_pd
+from .random_gen import rand_bq, rand_line, rand_quat, rand_rational
