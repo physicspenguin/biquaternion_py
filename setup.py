aa6dd4d3ebc7f6f6cabc4a0d09be74bbed15134e
Daren Thimm <daren.thimm@uibk.ac.at>
2023-10-02 18:09:14 +0200

    publishing setup

--- setup.py
+++ setup.py
@@ -2,23 +2,24 @@
 
 from setuptools import setup
 
-me = "Daren Thimm"
+me = "Daren A. Thimm"
 my_email = "daren.thimm@uibk.ac.at"
 setup(
     name="biquaternion-py",
-    version="1.0.0",
+    version="0.1.0",
     packages=[
         "biquat_py",
     ],
-    # license="BSD-3-Clause License",
+    license="GPL v3",
+    license_files="LICENSE",
     url="https://git.uibk.ac.at/c8441225/biquaternion_py",
     author=me,
     author_email=my_email,
     maintainer=me,
     maintainer_email=my_email,
     description="Implementation of general biquaternion algebras.",
-    # long_description=open("./README.md").read(),
-    # long_description_content_type="text/markdown",
+    long_description=open("./README.md").read(),
+    long_description_content_type="text/markdown",
     install_requires=[
         "numpy==1.24.3",
         "setuptools==67.7.0",
